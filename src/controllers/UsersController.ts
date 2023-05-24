6623438a9db284e6aa5dcc0ae40731ef44dfb7ab
Gabriel LK <gabrielfelipe941@yahoo.com.br>
2023-05-24 20:05:40 -0300

    more tables

--- src/controllers/UsersController.ts
+++ src/controllers/UsersController.ts
@@ -1,8 +1,43 @@
 import { connection as knex } from "../database/knex";
+import { hash } from "bcrypt";
+import { Request, Response } from "express";
 import { AppError } from "../utils/AppError";
 
+import { UserProps } from "../types/User";
+
 class UsersController {
-  create() {}
+  async create(req: Request, res: Response) {
+    const { name, username, password, email, drive_license }: UserProps =
+      req.body;
+
+    if (!name || !username || !password || !email || !drive_license) {
+      throw new AppError("Preencha todos os campos");
+    }
+
+    const verifyEmail = await knex("users").where({ email }).first();
+
+    if (verifyEmail) {
+      throw new AppError("Email já existe");
+    }
+
+    const user_pass = await hash(password, 8);
+
+    await knex("users").insert({
+      name,
+      username,
+      password: user_pass,
+      email,
+      drive_license,
+    });
+
+    return res.json({ message: "Usuario cadastrado" });
+  }
+
+  async show(req: Request, res: Response) {
+    const allUsers: UserProps[] = await knex("users");
+
+    return res.json(allUsers);
+  }
 }
 
 export { UsersController };
---
+++ src/types/User.ts
@@ -0,0 +1,10 @@
+export interface UserProps {
+  id: number;
+  name: string;
+  username: string;
+  password: string;
+  email: string;
+  drive_license: string;
+  is_admin: boolean;
+  created_at: Date;
+}
