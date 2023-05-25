2594c5c66c3060560eacdc51aeb64d028bf1c642
Gabriel LK <gabrielfelipe941@yahoo.com.br>
2023-05-25 16:00:12 -0300

    Car_Image & Car_Category created and updated

--- src/database/knex/migrations/20230524001305_createCarsImage.ts
+++ src/database/knex/migrations/20230524001305_createCarsImage.ts
@@ -3,7 +3,11 @@ import { Knex } from "knex";
 export async function up(knex: Knex): Promise<void> {
   return await knex.schema.createTable("cars_image", (table) => {
     table.increments("id");
-    table.integer("car_id").references("id").inTable("cars");
+    table
+      .integer("car_id")
+      .references("id")
+      .inTable("cars")
+      .onDelete("CASCADE");
     table.text("image_name").defaultTo(null);
     table.timestamp("created_at").defaultTo(knex.fn.now());
   });
